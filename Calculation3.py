bbc642a69d34f50c8f35039648d871cbf3d14771
shashi0101 <shash150104@gmail.com>
2026-04-08 10:49:41 +0530

    Add files via upload

---
+++ Calculation3.py
@@ -0,0 +1,6 @@
+#Calculation3
+from NumericCalculation import calculatesum
+m=10
+n=20
+print("sum of, m ,"and", n ,"is:",calculatesum(m,n))
+print("multiplication of, m ,"and", n ,"is:",calculatemultiplcation(m,n))
