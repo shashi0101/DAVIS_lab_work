35d8dcf3aa07dabfd5da0baf7d410c92eca2d3f1
shashi0101 <shash150104@gmail.com>
2026-04-08 10:30:39 +0530

    Add files via upload

---
+++ NumericCalculation.py
@@ -0,0 +1,23 @@
+# NumericCalculation
+
+#function to calculate addition of two numbers
+def calculatesum(x, y):
+    return (x + y)
+
+#function to calculate subtraction of two numbers
+def calculatedifference(x, y):
+    return (x - y)
+
+#function to calculate multiplication of two numbers
+def calculatemultiplication(x, y):
+     return (x * y)
+
+#function to calculate division of two numbers
+def calculatedivision(x, y):
+       if y == 0:
+        return "Error! Division by zero."
+    return (x / y)
+
+#function to calculate modulus of two numbers
+def calculatemodulus(x, y):
+       return(x % y)
