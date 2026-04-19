76608343e4985aa6e0fe0fd65ba69c89d8d7fc56
shashi0101 <shash150104@gmail.com>
2026-04-19 22:17:39 +0530

    Delete Calculation3.py

--- Calculation3.py
+++
@@ -1,6 +0,0 @@
-#Calculation3
-from NumericCalculation import calculatesum
-m=10
-n=20
-print("sum of, m ,"and", n ,"is:",calculatesum(m,n))
-print("multiplication of, m ,"and", n ,"is:",calculatemultiplcation(m,n))
