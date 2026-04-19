e5c19122bf05105ad77009e528617d7b03120868
shashi0101 <shash150104@gmail.com>
2026-04-19 22:17:23 +0530

    Delete Calculation2.py

--- Calculation2.py
+++
@@ -1,9 +0,0 @@
-#Calculation2.py
-from NumericCalculation import*
-m=10
-n=20
-print("sum of",m ,"and", n ,"is:",calculatesum(m,n))
-print("difference of",m ,"and", n ,"is:",calculatedifference(m,n))
-print("multiplication of",m ,"and", n ,"is:",calculatemultiplcation(m,n))
-print("division of",m ,"and", n ,"is:",calculatedivision(m,n))
-print("modulus of",m ,"and", n ,"is:",calculatemodulus(m,n))
